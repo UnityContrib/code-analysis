494673e1acc81eb3e1c39cde8e55995fd35f1ab3
Robin Theilade <robin.theilade@gmail.com>
2015-09-16 20:56:08 +0200

    Update for first nuget release

--- Code/UnityContrib.CodeAnalysis/UnityContrib.CodeAnalysis/Properties/AssemblyInfo.cs
+++ Code/UnityContrib.CodeAnalysis/UnityContrib.CodeAnalysis/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2015.09.13.2134")]
-[assembly: AssemblyFileVersion("2015.09.13.2134")]
+[assembly: AssemblyVersion("2015.09.16.2035")]
+[assembly: AssemblyFileVersion("2015.09.16.2035")]
